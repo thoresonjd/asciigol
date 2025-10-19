60d2fb288334c96f22d579cdfdafbe0d072b370c
Justin Thoreson <thoresonjd@gmail.com>
2025-10-18 17:40:00 -0700

    Create script that runs all invalid configuration files

---
+++ scripts/run_invalid_configs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Run this from the root of the project
+
+INVAL_CONFIG_DIR="./config/invalid"
+ASCIIGOL="asciigol"
+ASCIIGOL_BIN="./bin/$ASCIIGOL"
+
+if [ ! -d "$INVAL_CONFIG_DIR" ]; then
+	echo "$INVAL_CONFIG_DIR not found"
+	exit
+fi
+
+if [ ! -f "$ASCIIGOL_BIN" ]; then
+	make $ASCIIGOL
+fi
+
+for FILE in "$INVAL_CONFIG_DIR"/*; do
+	if [ -f "$FILE" ]; then
+		$ASCIIGOL_BIN --file=$FILE
+	fi
+done
+
