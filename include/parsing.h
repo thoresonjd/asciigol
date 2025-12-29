e836ac358722604400398bd47549c3d7efe20119
Justin Thoreson <thoresonjd@gmail.com>
2025-12-29 11:37:12 -0800

    Improve function comments in parsing.h

--- include/parsing.h
+++ include/parsing.h
@@ -13,33 +13,34 @@
 #include <stdint.h>
 
 /**
- * @brief Moves a string pointer forward passed a specified prefix
- * @param string The string in which the prefix is skipped
- * @param prefix The prefix to skip
+ * @brief Move a string pointer forward passed a specified prefix.
+ * @param string[in,out] The string in which the prefix is skipped.
+ * @param prefix[in] The prefix to skip.
+ * @return True if parsing succeeded, false otherwise.
  */
 bool skip_prefix(char** string, const char* prefix);
 
 /**
- * @brief Parses a 8-bit unsigned integer from string
- * @param arg The argument to parse
- * @param value Address to give the parsed uint8 to
- * @return true if parsing succeeded, false otherwise
+ * @brief Parse a 8-bit unsigned integer from string.
+ * @param[in] arg The argument to parse.
+ * @param[out] value The parsed uint8.
+ * @return True if parsing succeeded, false otherwise.
  */
 bool parse_uint8(const char* const arg, uint8_t* value);
 
 /**
- * @brief Parses a 16-bit unsigned integer from string
- * @param arg The argument to parse
- * @param value Address to give the parsed uint16 to
- * @return true if parsing succeeded, false otherwise
+ * @brief Parse a 16-bit unsigned integer from string.
+ * @param[in] arg The argument to parse.
+ * @param[out] value The parsed uint16.
+ * @return True if parsing succeeded, false otherwise.
  */
 bool parse_uint16(const char* const arg, uint16_t* value);
 
 /**
- * @brief Parses a character from a string
- * @param arg The argument to parse
- * @param character Address to give the parsed character to
- * @return true if parsing succeeded, false otherwise
+ * @brief Parse a character from a string.
+ * @param[in] arg The argument to parse.
+ * @param[out] character The parsed char.
+ * @return True if parsing succeeded, false otherwise.
  */
 bool parse_char(const char* const arg, char* character);
 
